27fd4570cfbe3e459a560867ac49c7594868620b
LotteMarit <58168449+Lotte-Marit@users.noreply.github.com>
2021-09-29 07:30:57 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+var x, y, vx, vy
+
+function setup() {
+  createCanvas(500, 400);
+  
+  
+  y = 320;
+  x =  40;
+ 
+}
+
+
+
+function draw() {
+  background(225);
+
+  
+  fill('green')
+  rect(380,270,50,50); 
+  rect(370,210,50,50);
+  rect(300,250,50,50)
+ 
+  fill('red')
+  circle(x,y,15);
+  
+ 
+}
+
+
+
+
